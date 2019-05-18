619cef66a4279f7344a1abfa25d09f1c7db7fa4e
unknown <archivecoderzero@gmail.com>
2019-05-18 10:18:55 -0700

    added new api key

--- assets/javascript/imageApiInCarousel.js
+++ assets/javascript/imageApiInCarousel.js
@@ -5,16 +5,11 @@ $( document ).ready(function() {
 
 var apiKey = "user_key=dd6dab64b179a2bb9a93127562835ed8";
 
-var searchResult = "";
-var corbEnabler = "https://cors-anywhere.herokuapp.com/";
-var queryOrgHunter = "https://data.orghunter.com/v1/charitysearch?"
-var twitterApiTrending = "https://api.twitter.com/1.1/trends/place.json?id=1"
-var trendSpotterKey1 = "ae8480dff3fa343b6d215da041939e3e"
-var backupKeyTrendspottr = "3bdb237763a5006354063055aa678cce";
-var trendSpotter = "http://api.trendspottr.com/v1.5/search?key=" +backupKeyTrendspottr+ "&q=?"
-var nytSearch = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&query=disaster&api-key=R1a31F4tBjCUaM2ho8GtIFsrSdtXt30M";
+var trendSpotterKey1 = "7743da92de8ab506139a44b7a092ca95"
+var backupKeyTrendspottr = "1582e1c19ebc9c57de3aa64745c46068";
+
+var trendSpotter = "http://api.trendspottr.com/v1.5/search?key=" +trendSpotterKey1+ "&q=?"
 var trendSpotterWord = "charity";
-var googleCustomSearchAPI = "AIzaSyBjbsMhY3Ex-VPnna_MT9VkGJnyZFfzKqE"
 var test = trendSpotter + trendSpotterWord ;
 
 
