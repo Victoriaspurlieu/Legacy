72b4efe79197085429a865c71531454769c7fc1a
unknown <archivecoderzero@gmail.com>
2019-05-18 18:37:52 -0700

    added non api version

--- assets/javascript/imageApiInCarousel.js
+++ assets/javascript/imageApiInCarousel.js
@@ -17,34 +17,52 @@ var queryURL = test;
 
 // WORKING CODE FOR TRENDSPOTTER -- START 1 ---->
 
-            $.ajax({
-                url: queryURL,
-                method: "GET",
-            }).then(function (response) {
-                // console.log(test)
-                for(i=0;i<7;i++){
+//             $.ajax({
+//                 url: queryURL,
+//                 method: "GET",
+//             }).then(function (response) {
+//                 // console.log(test)
+//                 for(i=0;i<7;i++){
 
-                    var trendingResult = response.results.hashtags[i].value;
-                    var trendingTopics = $("<h2>").text(trendingResult);
-                    $("#testDiv").append(trendingTopics)
-}
+//                     var trendingResult = response.results.hashtags[i].value;
+//                     var trendingTopics = $("<h2>").text(trendingResult);
+//                     $("#testDiv").append(trendingTopics)
+// }
 
 // WORKING CODE FOR TRENDSPOTTER -- END 1---->
 
 
-        var imageQuery1 = response.results.hashtags[0].value;
+
+// VARIABLE FOR TRENDSPOTTER API RESULT 1 START -- >
+
+        // var imageQuery1 = response.results.hashtags[0].value;
+
+// VARIABLE FOR TRENDSPOTTER API RESULT 1 END -- >
+ 
+
+        // NO API VERSION WORD 1 START---- >
+        var imageQuery1 = "#charity"
+        // NO API VERSION WORD 1 END---- >
+
         var queryDiv1 = $("<div class='onclickQuery'>");
-        // var imageQuery1 = "#charity"
         var imageQuery1Word = imageQuery1.slice(1);
         console.log(imageQuery1Word)
         queryDiv1.attr("topicQuery",imageQuery1Word)
         queryDiv1.html(imageQuery1);
         $("#hashtag1").prepend(queryDiv1);
-        
 
-        var imageQuery2 = response.results.hashtags[1].value;
+
+
+
+        
+        // NO API VERSION WORD 2 START---- >
+        var imageQuery2 = "#shelter"
+        // NO API VERSION WORD 2 END---- >
+
+          // var imageQuery2 = response.results.hashtags[1].value;
+
         var queryDiv2 = $("<div class='onclickQuery'>");
-        // var imageQuery2 = "#helping"
+
         var imageQuery2Word = imageQuery2.slice(1);
         console.log(imageQuery1Word)
         queryDiv2.attr("topicQuery",imageQuery2Word)
@@ -52,45 +70,67 @@ var queryURL = test;
         $("#hashtag2").prepend(queryDiv2);
 
 
-        var imageQuery3 = response.results.hashtags[2].value;
+        // NO API VERSION WORD 3 START---- >
+        var imageQuery3 = "#house"
+        // NO API VERSION WORD 3 END---- >
+
+          // var imageQuery3 = response.results.hashtags[2].value;
+
         var queryDiv3 = $("<div class='onclickQuery'>");
-        // var imageQuery3 = "#savetheworld"
         var imageQuery3Word = imageQuery3.slice(1);
         console.log(imageQuery3Word)
         queryDiv3.attr("topicQuery",imageQuery3Word)
         queryDiv3.html(imageQuery3);
         $("#hashtag3").prepend(queryDiv3);
 
-        var imageQuery4 = response.results.hashtags[3].value;
+         // NO API VERSION WORD 2 START---- >
+        var imageQuery4 = "#adopt"
+        // NO API VERSION WORD 2 END---- >
+    
+          // var imageQuery4 = response.results.hashtags[3].value;
+
+
         var queryDiv4 = $("<div class='onclickQuery'>");
-        // var imageQuery4 = "#animals"
         var imageQuery4Word = imageQuery4.slice(1);
         console.log(imageQuery4Word)
         queryDiv4.attr("topicQuery",imageQuery4Word)
         queryDiv4.html(imageQuery4);
         $("#hashtag4").prepend(queryDiv4);
         
-        var imageQuery5 = response.results.hashtags[4].value;
+        // var imageQuery5 = response.results.hashtags[4].value;
+
+        // NO API VERSION WORD 5 START---- >
+        var imageQuery5 = "#hunger"
+        // NO API VERSION WORD 5 END---- >
+
+
         var queryDiv5 = $("<div class='onclickQuery'>");
-        // var imageQuery5 = "#animalshelter"
         var imageQuery5Word = imageQuery5.slice(1);
         console.log(imageQuery5Word)
         queryDiv5.attr("topicQuery",imageQuery5Word)
         queryDiv5.html(imageQuery5);
         $("#hashtag5").prepend(queryDiv5);
 
-        var imageQuery6 = response.results.hashtags[5].value;
+          // var imageQuery6 = response.results.hashtags[5].value;
+
+        // NO API VERSION WORD 6 START---- >
+        var imageQuery6 = "#poverty"
+        // NO API VERSION WORD 6 END---- >
         var queryDiv6 = $("<div class='onclickQuery'>");
-        // var imageQuery6 = "#poverty"
         var imageQuery6Word = imageQuery6.slice(1);
         console.log(imageQuery1Word)
         queryDiv6.attr("topicQuery",imageQuery6Word)
         queryDiv6.html(imageQuery6);
         $("#hashtag6").prepend(queryDiv6);
 
-        var imageQuery7 = response.results.hashtags[6].value
+        // NO API VERSION WORD 6 START---- >
+        var imageQuery7 = "#world"
+        // NO API VERSION WORD 7 END---- >
+
+        // var imageQuery7 = response.results.hashtags[6].value
+
+
         var queryDiv7 = $("<div class='onclickQuery'>");
-        // var imageQuery7 = "#charity"
         var imageQuery7Word = imageQuery7.slice(1);
         console.log(imageQuery1Word)
         queryDiv7.attr("topicQuery",imageQuery7Word)
@@ -354,11 +394,11 @@ var queryURL = test;
 
 
 
-    // CLOSING FOR TRENDSPOTTER DIV
+    // CLOSING FOR API TRENDSPOTTER VERSION --- >
 
-});
+// });
 
-    // CLOSING FOR TRENDSPOTTER DIV
+    // CLOSING FOR API TRENDSPOTTER VERSION --->
 
     function randerNextPage () {
       clickQuery =  $(this).attr("topicQuery") ;
